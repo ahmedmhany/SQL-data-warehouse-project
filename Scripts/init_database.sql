6f5b7eb5df0138218b004a57f6764743c7624ea5
Ahmed Mhany <ahmedmhany101@gmail.com>
2025-10-27 14:38:56 +0300

    Create init_database.sql

---
+++ Scripts/init_database.sql
@@ -0,0 +1,15 @@
+--  Create databadse "Data warehouse"--
+ use master ;
+
+ Create database DataWarehouse ;
+ Go
+ Use DataWarehouse;
+ Go
+ --Create Schemas--
+ Create schema Bronze;
+ Go
+ Create schema Silver;
+ GO
+ Create schema Gold;
+ Go
+ 
